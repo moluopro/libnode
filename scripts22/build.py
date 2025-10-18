3f82793ccb03a103d0cc215347e094142fe34526
moluopro <moluopro@qq.com>
2025-10-19 02:16:15 +0800

    rm vs2019 in scripts22

---
+++ scripts22/build.py
@@ -0,0 +1,25 @@
+assert __name__ == "__main__"
+
+import sys
+import os
+import subprocess
+import shutil
+
+from . import config
+
+os.chdir('node-{}'.format(config.nodeVersion))
+
+configureArgvs = [ '--enable-static' ] + config.configFlags
+
+if sys.platform == 'win32':
+    env = os.environ.copy()
+    env['config_flags'] = ' '.join(configureArgvs)
+
+    args = ['cmd', '/c', 'vcbuild.bat']
+    if config.x86:
+        args.append('x86')
+    subprocess.check_call(args, env=env)
+    
+else:
+    subprocess.check_call([ sys.executable, 'configure.py' ] + configureArgvs)
+    subprocess.check_call(['make', '-j4'])
