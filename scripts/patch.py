75a537cc6295f57ba5b1a132074305481a24c088
moluopro <moluopro@qq.com>
2025-10-19 23:08:20 +0800

    移除过时的patch补丁

--- scripts/patch.py
+++ scripts/patch.py
@@ -5,5 +5,4 @@ import shutil
 
 os.chdir('node-{}'.format(config.nodeVersion))
 
-shutil.copytree('../patch/node', '.', dirs_exist_ok=True)
 subprocess.check_call(['patch', '-p1', '-i', '../patch/node.patch'])
