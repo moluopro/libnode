8354bbadfefcf12c9a5054c672fa3f74612ba955
moluopro <moluopro@qq.com>
2025-10-22 11:24:23 +0800

    修改补丁路径

--- bash/android-static.sh
+++ bash/android-static.sh
@@ -38,7 +38,7 @@ ls $HOME/android-ndk-r27d/toolchains/llvm/prebuilt/linux-x86_64/bin/clang
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-git apply patch/node22.patch
+git apply $WORKSPACE/patch/node22.patch
 
 bash ./android-configure "$HOME/android-ndk-r27d" 24 arm64
 
